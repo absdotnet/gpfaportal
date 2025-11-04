47e96c12c028dff1266b341aa40ca9ccb5d23722
absdotnet <absdotnetservices@gmail.com>
2025-11-04 10:58:31 -0800

    Initial Commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'GPFA Portal';
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'GPFA Portal for Brokers';
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
